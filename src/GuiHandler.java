1f2e1b3e57cf45766bef52af3819b63bc95dd2b4
Kenan Ozdemir <k.ozdemir@student.uliege.be>
2019-05-16 19:20:02 +0200

    Documentation

--- src/GuiHandler.java
+++ src/GuiHandler.java
@@ -8,30 +8,37 @@ public class GuiHandler implements ExplorerEventsHandler {
 
 	private ExplorerSwingView esv;
 	private String log = "";
+	private boolean printInFile;
 	
     GuiHandler(String[] args) throws NullHandlerException {
         this.esv = new ExplorerSwingView(this);
         
-        try {
+		try {
         	// First step to do before anything !!! 
             this.esv.setRootNode(new FolderNode("Root")); // set the root node with a silly "A" object
         } catch (RootAlreadySetException e) {
             e.printStackTrace();
         }
+
+		if(args == "log")
+			this.printInFile = true;
+        
     }
 
 	public void createAliasEvent(Object selectedNode) {
 		NodeType newAlias;
-		//perform the requested manipulation on selectedNode and see if it is allowed
+
+		//Perform the requested manipulation on selectedNode and check if it is allowed
 		if((newAlias = ((NodeType)selectedNode).createAlias()) != null){
-			//if it is, modify the JTree and add the new node to the parent folder
 			try {
+				//Modify the JTree by adding the new node to the parent folder and check for exceptions
     	        FolderNode parent = (FolderNode)this.esv.addNodeToParentNode(newAlias);
 				parent.addNodeInFolder(newAlias);
-				Logger newLog = new DateSystem(new UserSystem(new OsSystem(new AliasEvent(new EndLog()))));
-				newLog.log();
-				System.out.println(newLog.getLog());
-				this.log = this.log + (new String(newLog.getLog()));
+
+				//Update the logs
+				String newLog = (new DateSystem(new UserSystem(new OsSystem(new CopyEvent(new EndLog()))))).log();
+				this.log = this.log + (new String(newLog));
+				
 				esv.refreshTree();
 			} catch (NoSelectedNodeException | NoParentNodeException e) {
 				e.printStackTrace();
@@ -39,7 +46,7 @@ public class GuiHandler implements ExplorerEventsHandler {
         	}
 		}
 		else{
-			//if it is not, show an error popup window
+			//Show an error popup window
 			esv.showPopupError("Error: cannot create alias for this type of element.\n");
 			return;
 		}
@@ -47,29 +54,30 @@ public class GuiHandler implements ExplorerEventsHandler {
 
 	public void createArchiveEvent(Object selectedNode) {
 
-		//get user input information to create the archive
+		//Get the user's inputs to create the archive
 		String name = esv.displayArchiveWindow1();
 		String extension = esv.displayArchiveWindow2();
 		int compressionLVL = esv.displayArchiveWindow3();
 
-		//verify if all the data was successfully entered
+		//Verify if all the data was successfully entered
 		if(name == null || extension == null || compressionLVL < 0 || esv.isRootNodeSelected()){
-			//if it is not, show an error popup window
+			//If it is not, show an error popup window
 			esv.showPopupError("Error: operation 'create archive' has been cancelled or the selected node is the root.\n");
 			return;
 		}
 
-		//perform the requested manipulation on selectedNode and see if it is allowed
+		//Perform the requested manipulation on selectedNode and check if it is allowed
 		NodeType newArchive;
 		if((newArchive = ((NodeType)selectedNode).createArchive(name, extension, compressionLVL)) != null){
-			//if it is, modify the JTree and add the new node to the parent folder
 			try {
+				//Modify the JTree by adding the new node to the parent folder and check for exceptions				
     	        FolderNode parent = (FolderNode)this.esv.addNodeToParentNode(newArchive);
 				parent.addNodeInFolder(newArchive);
-				Logger newLog = new DateSystem(new UserSystem(new OsSystem(new ArchiveEvent(new EndLog()))));
-				newLog.log();
-				System.out.println(newLog.getLog());
-				this.log = this.log + (new String(newLog.getLog()));
+
+				//Update the logs
+				String newLog = (new DateSystem(new UserSystem(new OsSystem(new CopyEvent(new EndLog()))))).log();
+				this.log = this.log + (new String(newLog));
+
 				esv.refreshTree();
 			} catch (NoSelectedNodeException | NoParentNodeException e) {
 				e.printStackTrace();
@@ -84,9 +92,13 @@ public class GuiHandler implements ExplorerEventsHandler {
 	}
 
 	private void createCopyFolderInJTree(Object selectedNode, int level){
+		//Select the list of the selected folder
 		Node currentNode = ((FolderNode)selectedNode).getContent();
 		NodeType currentContent;
+
+		//Browsing that list
 		while(currentNode != null){
+			//Adding the current element for the JTree component
 			currentContent = currentNode.getContent();
 			try{
 				esv.addNodeToLastInsertedNode(currentContent, level);
@@ -98,67 +110,68 @@ public class GuiHandler implements ExplorerEventsHandler {
 			if(currentContent instanceof FolderNode){
 				this.createCopyFolderInJTree(currentContent, level + 1);
 			}
+
 			currentNode = currentNode.getNext();
 		}
 
 	}
 
 	public void createCopyEvent(Object selectedNode){
-		//verify that the selected node is not the root node
+		//Verify that the selected node is not the root node
 		if (esv.isRootNodeSelected()){
 			//if it is report the error with a popup window
 			esv.showPopupError("Error: cannot do a copy of the root.");
 			return;
 		}
-		//perform the requested manipulation on selectedNode and see if it is allowed
+		
+		//Perform the requested manipulation on selectedNode and check if it is allowed
 		NodeType newNode = ((NodeType)selectedNode).copyNode();
 		if(newNode == null){
 			//if it is not, show an error popup window
 			esv.showPopupError("Error: copying element.\n");
 			return;
 		}
-		//if it is, modify the JTree and add the new node to the parent folder	
 		try {
+			//Modify the JTree component by adding the new node to the parent folder	
 			((FolderNode)(this.esv.addNodeToParentNode(newNode))).addNodeInFolder(newNode);
 			if (selectedNode instanceof FolderNode){
 				this.createCopyFolderInJTree(selectedNode, 1);
 			}
-			Logger newLog = new DateSystem(new UserSystem(new OsSystem(new CopyEvent(new EndLog()))));
-			newLog.log();
-			System.out.println(newLog.getLog());
-			this.log = this.log + (new String(newLog.getLog()));
+
+			//Update the logs
+			String newLog = (new DateSystem(new UserSystem(new OsSystem(new CopyEvent(new EndLog()))))).log();
+			this.log = this.log + (new String(newLog));
+
 			esv.refreshTree();
-		} catch (NoSelectedNodeException e) {
+		} catch (NoSelectedNodeException | NoParentNodeException e) {
 			e.printStackTrace();
 			esv.showPopupError("Error: copying element.\n");
-    	} catch(NoParentNodeException p){
-			p.printStackTrace();
-			esv.showPopupError("Error: the selected node has no parent.fg\n");
 		}
 	}
 
 
 	public void createFileEvent(Object selectedNode) {
-
-		//get user input information to create the archive
+		//Get the user's inputs to create the file
 		String[] propertiesFile = this.esv.fileMenuDialog();
-		//verify if all the data was successfully entered
+
+		//Verify if all the data was successfully entered
 		if(propertiesFile == null && (propertiesFile[0] == null || propertiesFile[1] == null)){
 			//if it is not, show an error popup window
 			esv.showPopupError("Error: operation 'create file' has been cancelled. \n");
 			return;
 		}
 
-		//perform the requested manipulation on selectedNode and see if it is allowed
+		//Perform the requested manipulation on selectedNode and check if it is allowed
 		NodeType newFile;
 		if((newFile = ((NodeType)selectedNode).createFile(propertiesFile[0], propertiesFile[1])) != null){
-			//if it is, modify the JTree and add the new node to the selected folder	
 			try {
+				//Modify the JTree by adding the new node to the selected folder	
     	        this.esv.addNodeToSelectedNode(newFile);
-				Logger newLog = new DateSystem(new UserSystem(new OsSystem(new FileEvent(new EndLog()))));
-				newLog.log();
-				System.out.print(newLog.getLog());
-				this.log = this.log + (new String(newLog.getLog()));
+
+				//Update the logs				
+				String newLog = (new DateSystem(new UserSystem(new OsSystem(new CopyEvent(new EndLog()))))).log();
+				this.log = this.log + (new String(newLog));
+
 				esv.refreshTree();
 			} catch (NoSelectedNodeException e) {
             	e.printStackTrace();
@@ -173,24 +186,27 @@ public class GuiHandler implements ExplorerEventsHandler {
 	}
 
 	public void createFolderEvent(Object selectedNode) {
-		//get user input information to create the archive
+		//Get the user's input to create the folder
 		String nameFolder = this.esv.folderMenuDialog();
-		//verify if all the data was successfully entered
+
+		//Verify if all the data was successfully entered
 		if(nameFolder == null){
 			//if it is not, show an error popup window
 			esv.showPopupError("Error: operation 'create folder' has been cancelled. \n");
 			return;				
 		}
 
-		//perform the requested manipulation on selectedNode and see if it is allowed
+		//Perform the requested manipulation on selectedNode and check if it is allowed
 		NodeType newFolder;
 		if((newFolder = ((NodeType)selectedNode).createFolder(nameFolder)) != null){
-			//if it is, modify the JTree and add the new node to the selected folder	
 			try {
+				//Modify the JTree by adding the new node to the selected folder
 				this.esv.addNodeToSelectedNode(newFolder);
-				Logger newLog = new DateSystem(new UserSystem(new OsSystem(new FolderEvent(new EndLog()))));
-				System.out.print(newLog.log());
-				this.log = this.log + (new String(newLog.getLog()));
+
+				//Update the logs				
+				String newLog = (new DateSystem(new UserSystem(new OsSystem(new CopyEvent(new EndLog()))))).log();
+				this.log = this.log + (new String(newLog));
+
 				esv.refreshTree();
 			} catch (NoSelectedNodeException e) {
             	e.printStackTrace();
@@ -205,20 +221,34 @@ public class GuiHandler implements ExplorerEventsHandler {
 	}
 
 	public void doubleClickEvent(Object selectedNode) {
-		//get text area manager, clear the text area and add the information about the selected node
+		//Get text area manager, clear the text area and add the information about the selected node
 		TextAreaManager jt = this.esv.getTextAreaManager();
 		jt.clearAllText();
 		jt.appendText(((NodeType)selectedNode).getInfo(0));
 
-		//log the event
-		Logger newLog = new DateSystem(new UserSystem(new OsSystem(new DoubleClickEvent(new EndLog()))));
-		System.out.println(newLog.log());
-		this.log = this.log + (new String(newLog.getLog()));
+		//Update the logs		
+		String newLog = (new DateSystem(new UserSystem(new OsSystem(new CopyEvent(new EndLog()))))).log();
+		this.log = this.log + (new String(newLog));
 	}
 
 	public void eventExit() {
-		//print the complete events log in the specified file stream
-		System.out.println("final log:");
+
+		if(printInFile == true){
+			//Print the complete events log in the file "log.txt"
+			try {
+				File file = new File("log.txt");
+				FileWriter fileWriter = new FileWriter(file);
+				fileWriter.write("Final logs:\n" + log);
+				fileWriter.flush();
+				fileWriter.close();
+			} catch (IOException e) {
+			e.printStackTrace();
+			}
+		}
+		else{
+		//Print the complete events log in the terminal
+		System.out.println("Final logs:");
 		System.out.println(log);
+		}
 	}
 }

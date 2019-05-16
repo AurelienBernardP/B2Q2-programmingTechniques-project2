154e28a24d448b72b9cb62cb6e316be059d997f7
Aurélien Bernard <aurelien.bernard@student.uliege.be>
2019-05-16 23:37:05 +0200

    removed the useless log variable and addLog method

--- src/Logger.java
+++ src/Logger.java
@@ -1,15 +1,5 @@
 abstract class Logger{
-    protected String logs="";
 
-    /**
-     * Add the given string "newLogs" at the beginning of the string "logs" of the instance.
-     * @param newLogs :a string given to the current instance
-     * @return : "logs" from this instance concatenated to "newLogs" as a string
-     * 
-     */
-    public String addLog(String newLogs){
-       return this.logs = new String(newLogs + this.logs); 
-    }
 
     /**
      * Add an element, based on the instance from which it is called, at the beginning of the 
@@ -24,7 +14,6 @@ class EndLog extends Logger{
     @Override
     public String log(){
         
-
-        return super.addLog("\n");
+        return "\n";
     }
 } 
